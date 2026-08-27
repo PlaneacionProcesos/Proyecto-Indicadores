c9887679a77181c2b5d133ee351b3752b39c8eb0
Kevin Daniel Arango Algarra <kevinarangoalgarra31@gmail.com>
2026-08-27 12:13:02 -0500

    vercel despliegue

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ from componentes.sostenibilidad.callback_sostenibilidad import registrar_callbac
 
 
 app = Dash(__name__, suppress_callback_exceptions=True)
+server = app.server  # Instancia WSGI para Vercel / Gunicorn / Waitress
 
 
 # ========================================================================================
